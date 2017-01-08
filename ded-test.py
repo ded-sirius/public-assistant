99c0da793bacdfa94f8683c1528570af22119d40
Justin Le <lejustin.lv@gmail.com>
2017-01-07 21:50:37 -0800

    Initial

--- ded-test.py
+++ ded-test.py
@@ -1,6 +1,10 @@
 import logging
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question, session
+from flask_ask import Ask, statement, question
+
+# import sys
+# sys.path.insert(0, './lib/')
+from lib.dir import Directions
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -14,9 +18,21 @@ def new_session():
 
 @ask.intent("QueryIntent")
 
-def get_route():
-    directions = " ".join(["First path", "Second path"])
-    directions_msg = render_template('route', directions=directions)
+def get_route(origin, destination):
+    print '='*88
+
+    # origin = "walmart"
+    # destination = "sunset park"
+
+    print Directions
+    print ". ".join([origin, destination])
+    directions = Directions(origin, destination)
+    print directions
+
+    instructions = directions.instructions
+    print ". ".join(instructions)
+
+    directions_msg = render_template('route', directions = "Hello. Hello.")
     return statement(directions_msg)
 
 if __name__ == '__main__':
