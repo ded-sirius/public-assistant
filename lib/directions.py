fd3fdcf0ca0087aa2173c9e4817d4f0793b07ca1
Martin Jaime <martin.jaime-viveros@originate.com>
2017-01-07 20:11:37 -0800

    creates class for getting directions

--- lib/directions.py
+++ lib/directions.py
@@ -2,27 +2,34 @@
 import re
 import googlemaps
 from datetime import datetime
+from config import GOOGLE_API_KEY
+
+MODE = 'walking'
+
+class Directions:
+    def __init__(self, origin, destination):
+        self.instructions = []
+        self.time = 0
+        self.distance = 0
+        self.gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+        self.getDirections(origin, destination)
+        self.getInstructions()
+
+    def getDirections(self, origin, destination):
+        # Request directions via public transit
+        now = datetime.now()
+        self.directions_result = self.gmaps.directions(origin,
+                                             destination,
+                                             mode="transit",
+                                             departure_time=now)
+
+    def getInstructions(self):
+        steps = self.directions_result[0]['legs'][0]['steps']
+        for step in steps:
+            instruction = cleanhtml(step['html_instructions'])
+            self.instructions.append(instruction)
 
 def cleanhtml(raw_html):
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', raw_html)
     return cleantext
-
-def getDirections(origin, destination, maxDistance=0):
-    # Request directions via public transit
-    now = datetime.now()
-    directions_result = gmaps.directions("6795 S Edmond St #110, Las Vegas, NV 89118",
-                                         "UNLV",
-                                         mode="transit",
-                                         departure_time=now)
-
-def getTextDirections(gDirections):
-    directions = []
-    steps = gDirections[0]['legs'][0]['steps']
-    # output starting direction
-    #directions.append(steps['html_instructions'])
-    for step in steps:
-        instruction = cleanhtml(step['html_instructions'])
-        print instruction
-    return directions
-
